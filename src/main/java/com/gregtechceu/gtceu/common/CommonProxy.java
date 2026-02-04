9e5e35a85c79c8a390bb5a832c110d8115d36788
screret <68943070+screret@users.noreply.github.com>
2026-02-04 18:32:33 +0200

    Delete KeyBind

--- src/main/java/com/gregtechceu/gtceu/common/CommonProxy.java
+++ src/main/java/com/gregtechceu/gtceu/common/CommonProxy.java
@@ -53,7 +53,6 @@ import com.gregtechceu.gtceu.integration.kjs.GTCEuStartupEvents;
 import com.gregtechceu.gtceu.integration.kjs.GTRegistryInfo;
 import com.gregtechceu.gtceu.integration.kjs.events.MaterialModificationEventJS;
 import com.gregtechceu.gtceu.integration.map.WaypointManager;
-import com.gregtechceu.gtceu.utils.input.KeyBind;
 import com.gregtechceu.gtceu.utils.input.SyncedKeyMappings;
 
 import com.lowdragmc.lowdraglib.gui.factory.UIFactory;
@@ -194,7 +193,6 @@ public class CommonProxy {
         GTFeatures.init();
         GTFeatures.register();
         CustomBlockRotations.init();
-        KeyBind.init();
         SyncedKeyMappings.init();
         MachineOwner.init();
 
