16bd6f6db4c4a69fd22b38b3ab477a30f63caf80
screret <68943070+screret@users.noreply.github.com>
2026-04-21 12:04:48 +0300

    Make charging break progress mixin only apply if both items are GT tools

--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/MultiPlayerGameModeMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/MultiPlayerGameModeMixin.java
@@ -1,5 +1,6 @@
 package com.gregtechceu.gtceu.core.mixins.client;
 
+import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.tool.ToolHelper;
 
 import net.minecraft.client.Minecraft;
@@ -9,7 +10,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -44,16 +46,16 @@ public class MultiPlayerGameModeMixin {
         state.getBlock().destroy(level, pos, state);
     }
 
-    @Shadow
-    private ItemStack destroyingItem;
-    @Shadow
-    private BlockPos destroyBlockPos;
-
-    @Inject(method = "sameDestroyTarget", at = @At("RETURN"), cancellable = true)
-    private void gtceu$sameDestroyTarget(BlockPos pos, CallbackInfoReturnable<Boolean> cir,
-                                         @Local ItemStack itemstack) {
-        boolean gtmTarget = pos.equals(this.destroyBlockPos) && ItemStack.isSameItem(itemstack, this.destroyingItem) &&
-                !this.destroyingItem.shouldCauseBlockBreakReset(itemstack);
-        cir.setReturnValue(gtmTarget || cir.getReturnValue());
+    @WrapOperation(method = "sameDestroyTarget",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private boolean gtceu$sameDestroyTarget(ItemStack mainHandItem, ItemStack destroyingItem,
+                                            Operation<Boolean> original) {
+        // Fix Tool charging resetting block break progress
+        if (mainHandItem.getItem() instanceof IGTTool && destroyingItem.getItem() instanceof IGTTool) {
+            return ItemStack.isSameItem(mainHandItem, destroyingItem);
+        } else {
+            return original.call(mainHandItem, destroyingItem);
+        }
     }
 }
