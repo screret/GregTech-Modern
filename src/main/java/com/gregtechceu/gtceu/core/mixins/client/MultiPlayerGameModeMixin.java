0f0c5347c3f98b5741b6769decc08ac62a996b6b
screret <68943070+screret@users.noreply.github.com>
2026-04-21 15:57:07 +0300

    Not sure if this one works, though.

--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/MultiPlayerGameModeMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/MultiPlayerGameModeMixin.java
@@ -3,47 +3,39 @@ package com.gregtechceu.gtceu.core.mixins.client;
 import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.tool.ToolHelper;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
 
-    @Shadow
-    @Final
-    private Minecraft minecraft;
+    @WrapOperation(method = "destroyBlock",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/block/state/BlockState;onDestroyedByPlayer(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;ZLnet/minecraft/world/level/material/FluidState;)Z"))
+    private boolean gtceu$destroyBlock(BlockState state, Level level, BlockPos pos,
+                                       Player player, boolean willHarvest, FluidState fluidState,
+                                       Operation<Boolean> original) {
+        if (player.isShiftKeyDown()) {
+            return original.call(state, level, pos, player, willHarvest, fluidState);
+        }
 
-    @Inject(method = "destroyBlock", at = @At("HEAD"), cancellable = true)
-    private void gtceu$destroyBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        ItemStack mainHandItem = minecraft.player.getMainHandItem();
-        if (minecraft.player == null ||
-                minecraft.level == null ||
-                !ToolHelper.hasBehaviorsTag(mainHandItem) ||
+        ItemStack mainHandItem = player.getMainHandItem();
+        if (!ToolHelper.hasBehaviorsTag(mainHandItem) ||
                 ToolHelper.getAoEDefinition(mainHandItem).isZero() ||
-                minecraft.player.isShiftKeyDown() ||
-                !mainHandItem.isCorrectToolForDrops(minecraft.level.getBlockState(pos)))
-            return;
-
-        cir.setReturnValue(false);
-        Level level = minecraft.level;
-
-        if (level == null) return;
-        BlockState state = level.getBlockState(pos);
-
-        state.getBlock().destroy(level, pos, state);
+                !mainHandItem.isCorrectToolForDrops(state)) {
+            return original.call(state, level, pos, player, willHarvest, fluidState);
+        }
+        return true;
     }
 
     @WrapOperation(method = "sameDestroyTarget",
